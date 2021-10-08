3d1cb4a95ea72174a9be6830376af298e1f4b4ca
aleidinger <alina.leidinger@gmail.com>
2021-10-08 18:16:53 +0200

    first cleaned up try to be run on the server

--- attention_counts.py
+++ attention_counts.py
@@ -1,124 +1,12 @@
-import os
-import nltk, re, string
-import transformers
-import datasets
+import os, sys
+import re, string
 import pandas as pd
-import numpy as np
-from transformers import BertTokenizer, BertModel
-from datasets import load_dataset, DatasetDict, dataset_dict
-from nltk.tokenize import word_tokenize
-from nltk.text import ConcordanceIndex
+import torch
+from transformers import AutoTokenizer, AutoModel
+from datasets import load_dataset
+from typing import Dict
 
 # TODO think about setting up python 3.7 env as rochelle was using
-
-# need the corpus to get the concordances
-
-def nltk_preprocess(data, tokenize=True):
-    ''' 
-    input list of strings with each string being one article
-    '''
-    # took inspiration from: https://www.kaggle.com/rtatman/tutorial-getting-n-grams
-    tmp = ' '.join(data)
-    punctuationNoPeriod = "[" + re.sub("\.","",string.punctuation) + "]"
-    tmp = re.sub(punctuationNoPeriod, "", tmp).lower()
-    if tokenize:
-        tmp = word_tokenize(tmp)
-#    return tmp.split()
-    return tmp
-
-
-def find_concordance_custom(ci, word, width=80):
-        """
-        Find all concordance lines given the query word.
-        Provided with a list of words, these will be found as a phrase.
-        """
-        # rewrite find_concordance. http://www.nltk.org/_modules/nltk/text.html#ConcordanceIndex
-        if isinstance(word, list):
-            phrase = word
-        else:
-            phrase = [word]
-
-        half_width = (width - len(' '.join(phrase)) - 2) // 2
-        context = width // 4  # approx number of words of context
-
-        # Find the instances of the word to create the ConcordanceLine
-        concordance_list = []
-        offsets = ci.offsets(phrase[0])
-        for i, word in enumerate(phrase[1:]):
-            word_offsets = {offset - i - 1 for offset in ci.offsets(word)}
-            offsets = sorted(word_offsets.intersection(offsets))
-        if offsets:
-            for i in offsets:
-                query_word = " ".join(ci._tokens[i : i + len(phrase)])
-                # Find the context of query word.
-                left_context = ci._tokens[max(0, i - context) : i]
-                right_context = ci._tokens[i + len(phrase) : i + context]
-                # Create the pretty lines with the query_word in the middle.
-                left_print = " ".join(left_context)[-half_width:]
-                right_print = " ".join(right_context)[:half_width]
-                # The WYSIWYG line of the concordance.
-                line_print = " ".join([left_print, query_word, right_print])
-                # Create the ConcordanceLine
-#                 concordance_line = ConcordanceLine(
-#                     left_context,
-#                     query_word,
-#                     right_context,
-#                     i,
-#                     left_print,
-#                     right_print,
-#                     line_print,
-#                 )
-#                 concordance_list.append(concordance_line)
-                concordance_list.append(line_print)
-        return concordance_list
-
-def train_val_split(dat, validation_percentage=0.1):
-    ''' 
-    adapted from https://discuss.huggingface.co/t/how-to-split-main-dataset-into-train-dev-test-as-datasetdict/1090/2
-    simple function to split existing huggingface Dataset object. 
-    returns DatasetDict containing training and validation dataset
-    default split ratio 90-10
-    note that validation set is still named test as I am using datasets train_test_split method
-    '''
-    train_valid = dat.train_test_split(test_size=0.1)
-    # gather everyone to have a single DatasetDict
-    #train_valid_dict = DatasetDict({
-    #    'train': train_valid['train'],
-    #    'valid': train_valid['test']})
-    return train_valid
-
-def get_concordances(cat_of_interest, newspaper):
-    
-    # TODO make dataset path an input arg?
-
-    allthenews = load_dataset('csv', script_version='master', data_files=['../bias/data/external/archive/articles1.csv', '../bias/data/external/archive/articles2.csv', '../bias/data/external/archive/articles3.csv'], 
-                          column_names = ['Unnamed', 'id', 'title', 'publication', 'author', 'date', 'year', 'month', 'url', 'content'])
-    
-    # maybe do this for all newspapers simultaneously in the future, for now stick to one
-    #allthenews_dict = {newspaper : allthenews.filter(lambda example: example['publication']==newspaper).remove_columns(['Unnamed', 'title', 'publication', 'author', 'year', 'month', 'url', 'date']) for newspaper in newspapers}
-
-    # filter for 'newspaper' keep all samples (that were before in train)
-    try:
-        news = allthenews.filter(lambda example: example['publication']==newspaper).remove_columns(['Unnamed', 'title', 'publication', 'author', 'year', 'month', 'url', 'date'])['train']['content']
-    except:
-        raise Exception('You entered an invalid newspaper. Enter one of Breitbart, Fox News, Guardian or Reuters.')
-    
-    # tokenize using nltk
-    tokenized_news = nltk_preprocess(news)
-1
-    # allthebreitbart = allthenews.filter(lambda example: example['publication']=='Breitbart').remove_columns(['Unnamed', 'title', 'publication', 'author', 'year', 'month', 'url', 'date'])
-    # allthefox = allthenews.filter(lambda example: example['publication']=='Fox News').remove_columns(['Unnamed', 'title', 'publication', 'author', 'year', 'month', 'url', 'date'])
-    # allthereuters = allthenews.filter(lambda example: example['publication']=='Reuters').remove_columns(['Unnamed', 'title', 'publication', 'author', 'year', 'month', 'url', 'date'])
-    # alltheguardian = allthenews.filter(lambda example: example['publication']=='Guardian').remove_columns(['Unnamed', 'title', 'publication', 'author', 'year', 'month', 'url', 'date'])
-    # allthenews_dict = {'breitbart': allthebreitbart, 'fox': allthefox, 'reuters': allthereuters, 'guardian': alltheguardian}#, 'newyorker': allthenewyorker}
-    # allthenews_trainval = {newspaper : train_val_split(allthenews_dict[newspaper]['train']) for newspaper, dat in allthenews_dict.items()}
-    # tokenized_news = {newspaper: nltk_preprocess(allthenews_trainval[newspaper]['train']['content']) for newspaper in newspapers}
-    #tokenized_news = {newspaper: nltk_preprocess(allthenews_trainval[newspaper]['train']['content']) for newspaper in ['guardian']}
-
-    ci = ConcordanceIndex(tokenized_news)
-    concordances = find_concordance_custom(ci, cat_of_interest)
-    return concordances
-
 
 def group_tokens_to_ids(tokens, tokenizer):
     ''' return dict {token : id} if tokens is a list of tokens 
@@ -131,8 +19,22 @@ def group_tokens_to_ids(tokens, tokenizer):
         ids = {cat: {token: idx for token, idx in zip(tokens[cat], tokenizer.convert_tokens_to_ids(tokens[cat])) if idx != 100} for cat in tokens.keys()}
     return ids
 
+def group_texts(examples, block_size=128):
+    # Concatenate all texts.
+    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
+        # customize this part to your needs.
+    total_length = (total_length // block_size) * block_size
+    # Split by chunks of max_len.
+    result = {
+        k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+        for k, t in concatenated_examples.items()
+    }
+    result["labels"] = result["input_ids"].copy()
+    return result
 
-def attention_weighted_counts(concordances, reference_word, bert_model_path, layer_id, attention_head_id):
+def attention_weighted_counts(args: Dict):
     '''
     Given a category of interest (reference_word) and a list of strings that each contain reference word count the emotion words
     Counts are weighted by the attention when passing concordances through an LM
@@ -142,38 +44,67 @@ def attention_weighted_counts(concordances, reference_word, bert_model_path, lay
     attention_weighted_counts(concordances_fox_girl, 'girl', 'bert-base-uncased', emotionwords_dict, 11, 0)
     '''
 
-    # TODO change these to AutoTokenizer and AutoModelForMaskedLM later and the input arg from bert_model_path to model_path for clarity
-    print('--Calculating counts weighted by attention--')
-    tokenizer = BertTokenizer.from_pretrained(bert_model_path)#, do_lower_case=True)
-    model = BertModel.from_pretrained(bert_model_path, output_attentions=True)
-
-    print('--tokenizing concordances--')
-    tokenized_concordances = tokenizer(concordances, padding=True, return_tensors='pt')
-    token_type_ids = tokenized_concordances['token_type_ids']
-    input_ids = tokenized_concordances['input_ids']
-    
-    print('--getting attention--')
-    attentions = model(input_ids, token_type_ids=token_type_ids)[-1]
+    newspaper = str(args['--newspaper'])
+    reference_word = str(args['--reference_word'])
+    model_path = str(args['--model_path'])
+    layer_id = int(args['--layer_id'])
+    attention_head_id = int(args['--attention_head_id'])
 
     #load emotion words
     emotionwords = pd.read_excel('../data/external/NRC-Emotion-Lexicon-v0.92-In105Languages-Nov2017Translations.xlsx', usecols="A,DB:DK")
     emotions = list(emotionwords.columns[1:])
     emotionwords_dict = {emotion: list(emotionwords['English (en)'].loc[emotionwords[emotion]==1]) for emotion in emotions}
-    # cast series to list. change back if this breaks things
-    del emotionwords
+    print('--loaded emotion words--')
+
+    tokenizer = AutoTokenizer.from_pretrained(model_path)#, do_lower_case=True)
+    model = AutoModel.from_pretrained(model_path, output_attentions=True)
+    print('--defined model--')
+
+    news = load_dataset('csv', script_version='master', data_files=['../data/external/archive/articles1.csv', '../data/external/archive/articles2.csv', '../data/external/archive/articles3.csv'],
+                          column_names = ['Unnamed', 'id', 'title', 'publication', 'author', 'date', 'year', 'month', 'url', 'content']).remove_columns(['Unnamed', 'title', 'author', 'year', 'month', 'url', 'date'])
+    print('--loaded dataset--')
+
+    # filter for publication of interest
+    news_newspaper = news.filter(lambda example: example['publication']==newspaper).remove_columns(['publication'])
     
-    #cts = attention_weighted_counts(tokenized_concordances['input_ids'], tokenizer, attentions, reference_word, emotion_tokens, layer_id, attention_head_id)
-    #print(attentions.shape)
+    # find articles that contain the word
+    articles_w_ref_wd = news_newspaper.filter(lambda example: reference_word in example['content'])
+
+    # tokenize
+    def tokenize_function(examples):
+        return tokenizer(examples["content"])
     
-    # get id of identity_word that serves as reference point
-    ref_wd_id = tokenizer.convert_tokens_to_ids(reference_word)
+    articles_tokenized = articles_w_ref_wd.map(tokenize_function, batched=True, num_proc=4, remove_columns=['id', 'content'])
+
+    # split data into processable chunks
+    snippets = articles_tokenized.map(
+            group_texts,
+            batched=True,
+            batch_size=1000,
+            num_proc=4,
+        )
+
+    # find idx of reference word
+    idx = tokenizer.convert_tokens_to_ids(reference_word)
+
+    # discard snippets that don't have the reference word
+    snippets_wd = snippets['train'].filter(lambda example: idx in example['input_ids'])
+
+    # cast to tensor
+    input_ids = torch.tensor(snippets_wd['input_ids'])
+    token_type_ids = torch.tensor(snippets_wd['token_type_ids'])
+    print('--prepared concordances--')
+
+    # pass the chunks to the model to get attention
+    attentions = model(input_ids, token_type_ids=token_type_ids)[-1]
+    print('--calculated attentions--')
     
     # number of times identity word occurrs. should be length of text_snippets
-    N = len(concordances)
+    N = len(snippets_wd)
 
     # get list of lists with tuples (word_idx, attention from reference word to this word)
     print('--collecting counts--')
-    w_cts = [[(idx.item(), attention.item()/N) for idx, attention in zip(input_ids[sentence_id], attentions[layer_id][sentence_id, attention_head_id, input_ids[sentence_id].tolist().index(ref_wd_id),:])] for sentence_id in range(input_ids.shape[0])]
+    w_cts = [[(idx.item(), attention.item()/N) for idx, attention in zip(input_ids[sentence_id], attentions[layer_id][sentence_id, attention_head_id, input_ids[sentence_id].tolist().index(idx),:])] for sentence_id in range(input_ids.shape[0])]
         
     # flatten list
     w_cts = [item for sublist in w_cts for item in sublist]
@@ -183,24 +114,23 @@ def attention_weighted_counts(concordances, reference_word, bert_model_path, lay
     emotion_ids = group_tokens_to_ids(emotionwords_dict, tokenizer)
         
     print('--summing over emotion categories--')
-    sum_w_cts = {emotion: sum([sum([ct[1] for ct in w_cts if ct[0] == emotion_term_id]) for emotion_term, emotion_term_id in emotion_ids[emotion].items()]) for emotion in emotions}
+    sum_w_cts = {emotion: sum([sum([ct[1] for ct in w_cts if ct[0] == emotion_term_id]) for emotion_term_id in emotion_ids[emotion].values()]) for emotion in emotions}
     
     print('--finished weighted counts of emotion words--')
 
     return sum_w_cts
 
 
-def show_counts(newspapers, gps_of_interest, model_path, layer_id, attention_head_id):
+def show_counts(newspaper, reference_word, model_path, layer_id, attention_head_id):
     '''
     newspapers, gps_of_interest: str or list of strings with newspapers and groups, ie words designating different groups that we are interested in
     
     Example usage: show_counts('Guardian', 'girl', 'bert-base-uncased', 11, 0)
     '''
     # test that this works first
-    if type(newspapers) == str and type(gps_of_interest) == str and type(model_path) == str:
-        concordances = get_concordances(gps_of_interest, newspapers)
-        cts = attention_weighted_counts(concordances, gps_of_interest, model_path, layer_id, attention_head_id)
-        print(newspapers, gps_of_interest)
+    if type(newspapers) == str and type(reference_word) == str and type(model_path) == str:
+        cts = attention_weighted_counts(newspaper, reference_word, model_path, layer_id, attention_head_id)
+        print(newspapers, reference_word)
         print("{:<8} {:<15}".format('Emotion','Score'))
         for k, v in cts.items():
             print("{:<8} {:<15}".format(k, v))
