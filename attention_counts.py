b6494cd2d6a49bb99f5678aa2794b95ca40a71dd
aleidinger <alina.leidinger@gmail.com>
2021-10-13 15:26:36 +0200

    version up to date with server version

--- attention_counts.py
+++ attention_counts.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-import os, sys
+import os, sys, json
 import re, string
 import pandas as pd
 import torch
 from transformers import AutoTokenizer, AutoModel
 from datasets import load_dataset
 from typing import Dict
-import json
-# from docopt import docopt
-
-
 # TODO think about setting up python 3.7 env as rochelle was using
 
 def group_tokens_to_ids(tokens, tokenizer):
@@ -22,18 +18,19 @@ def group_tokens_to_ids(tokens, tokenizer):
     if type(tokens) == list:
         ids = {token: idx for token, idx in zip(tokens, tokenizer.convert_tokens_to_ids(tokens))}
     if type(tokens) == dict:
-        ids = {}
-        for cat in tokens.keys():
-            ids[cat] = {}
-            print(tokens[cat].values())
-            for token in tokens[cat].values(): # idx in zip(tokens[cat], tokenizer.convert_tokens_to_ids(tokens[cat])):
-                if token not in [tokenizer.unk_token, tokenizer.pad_token, tokenizer.cls_token, tokenizer.mask_token, tokenizer.sep_token]:
-                    ids[cat][token] = tokenizer.convert_tokens_to_ids(token)
+        ids = {cat: {token: idx for token, idx in zip(tokens[cat], tokenizer.convert_tokens_to_ids(tokens[cat])) if idx != 100} for cat in tokens.keys()}
+#   if type(tokens) == dict:
+#        ids = {}
+#        for cat in tokens.keys():
+#            ids[cat] = {}
+#            print(tokens[cat])
+#            for token in tokens[cat].values(): # idx in zip(tokens[cat], tokenizer.convert_tokens_to_ids(tokens[cat])):
+#                if token not in [tokenizer.unk_token, tokenizer.pad_token, tokenizer.cls_token, tokenizer.mask_token, tokenizer.sep_token]:
+#                    ids[cat][token] = tokenizer.convert_tokens_to_ids(token)
         # ids = {cat: {token: idx for token, idx in zip(tokens[cat], tokenizer.convert_tokens_to_ids(tokens[cat])) if idx != 100} for cat in tokens.keys()}
     return ids
 
 def group_texts(examples, block_size=128):
-
     # Concatenate all texts.
     concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
     total_length = len(concatenated_examples[list(examples.keys())[0]])
@@ -48,43 +45,41 @@ def group_texts(examples, block_size=128):
     result["labels"] = result["input_ids"].copy()
     return result
 
-# def attention_weighted_counts(args: Dict):
-# def attention_weighted_counts(newspaper='Breitbart', reference_word='girl', model_path='bert-base-uncased', layer_id=11, attention_head_id=0, save_name=''):
-def attention_weighted_counts(newspaper='Breitbart', reference_word='girl', model_path='bert-base-uncased', save_name=''):
 
+def attention_weighted_counts(newspaper='Guardian', reference_word='girl', model_path='bert-base-uncased', save_name=''):
     '''
     Given a category of interest (reference_word) and a list of strings that each contain reference word count the emotion words
     Counts are weighted by the attention when passing concordances through an LM
     use attention head number 'attention_head_id' in layer 'layer_id'.
     
     Example Usage:
-    attention_weighted_counts(concordances_fox_girl, 'girl', 'bert-base-uncased', emotionwords_dict, 11, 0)
+    attention_weighted_counts('Breitbart', 'girl', 'bert-base-uncased', 11, 0, breitbart_girl_bertbase)
     '''
 
-    # newspaper = str(args['--newspaper'])
-    # reference_word = str(args['--reference_word'])
-    # model_path = str(args['--model_path'])
-    # layer_id = int(args['--layer_id'])
-    # attention_head_id = int(args['--attention_head_id'])
-    # save_name = str(args['--save_name'])
+    #newspaper = str(args['--newspaper'])
+    #reference_word = str(args['--reference_word'])
+    #model_path = str(args['--model_path'])
+    #layer_id = int(args['--layer_id'])
+    #attention_head_id = int(args['--attention_head_id'])
+    #save_name = str(args['--save_name'])
 
     tokenizer = AutoTokenizer.from_pretrained(model_path)#, do_lower_case=True)
     model = AutoModel.from_pretrained(model_path, output_attentions=True)
-    print('--defined model--')
 
     #load emotion words
-    emotionwords = pd.read_excel('../bias/data/external/NRC-Emotion-Lexicon-v0.92-In105Languages-Nov2017Translations.xlsx', usecols="A,DB:DK")
+    emotionwords = pd.read_excel('./input_dir_wdcts/NRC-Emotion-Lexicon-v0.92-In105Languages-Nov2017Translations.xlsx', usecols="A,DB:DK")
     emotions = list(emotionwords.columns[1:])
+    #   emotionwords_dict = {emotion: list(emotionwords['English (en)'].loc[emotionwords[emotion]==1]) for emotion in emotions}
+
     emotionwords_dict = {emotion: [str(elt) for elt in list(emotionwords['English (en)'].loc[emotionwords[emotion]==1])] for emotion in emotions}
-    print('--loaded emotion words--')
+
 
     # emotion tokens to ids
     emotion_ids = group_tokens_to_ids(emotionwords_dict, tokenizer)
-    print('--mapped emotion word tokens to ids--')
 
-    news = load_dataset('csv', script_version='master', data_files=['../data/external/archive/articles1.csv', '../data/external/archive/articles2.csv', '../data/external/archive/articles3.csv'],
+    news = load_dataset('csv', script_version='master', data_files=['./input_dir_wdcts/articles1.csv', './input_dir_wdcts/articles2.csv', './input_dir_wdcts/articles3.csv'],
                           column_names = ['Unnamed', 'id', 'title', 'publication', 'author', 'date', 'year', 'month', 'url', 'content']).remove_columns(['Unnamed', 'title', 'author', 'year', 'month', 'url', 'date'])
-    print('--loaded dataset--')
+
 
     # filter for publication of interest
     news_newspaper = news.filter(lambda example: example['publication']==newspaper).remove_columns(['publication'])
@@ -115,18 +110,28 @@ def attention_weighted_counts(newspaper='Breitbart', reference_word='girl', mode
     # cast to tensor
     input_ids = torch.tensor(snippets_wd['input_ids'])
     token_type_ids = torch.tensor(snippets_wd['token_type_ids'])
-    print('--prepared concordances--')
 
     # pass the chunks to the model to get attention
     attentions = model(input_ids, token_type_ids=token_type_ids)[-1]
-    print('--calculated attentions--')
     
     # number of times identity word occurrs. should be length of text_snippets
+
     N = len(snippets_wd)
 
     # get list of lists with tuples (word_idx, attention from reference word to this word)
-    print('--collecting counts--')
-    # this gives a dict of shape {layer_id: {attention_head_id : {emotion: sum of weighted counts for words belonging to this emotion}}}
+    #w_cts = [[(idx.item(), attention.item()/N) for idx, attention in zip(input_ids[sentence_id], attentions[layer_id][sentence_id, attention_head_id, input_ids[sentence_id].tolist().index(idx),:])] for sentenc$
+
+    # flatten list
+    #w_cts = [item for sublist in w_cts for item in sublist]
+    
+    # emotion tokens to ids
+    #print('--mapping emotion word tokens to ids--')
+    #emotion_ids = group_tokens_to_ids(emotionwords_dict, tokenizer)
+
+    #print('--summing over emotion categories--')
+    #sum_w_cts = {emotion: sum([sum([ct[1] for ct in w_cts if ct[0] == emotion_term_id]) for emotion_term_id in emotion_ids[emotion].values()]) for emotion in emotions}
+    
+    #print('--finished weighted counts of emotion words--')
     outdict = {}
     for layer_id in range(12):# don't hard code this if I want to change to different architectures
         outdict[layer_id] = {}
@@ -147,15 +152,15 @@ def attention_weighted_counts(newspaper='Breitbart', reference_word='girl', mode
             outdict[layer_id]['sum'][emotion] = ct
             outdict[layer_id]['avg'][emotion] = ct/12
 
-    print('--finished weighted counts of emotion words--')
 
-    a_file = open(save_name+'weighted_counts.json', "w")
+    a_file = open('./test_output/'+save_name+'weighted_counts.json', "w")
 
     json.dump(outdict, a_file)
 
     a_file.close()
 
     return outdict
+
 
 
 # def show_counts(newspaper, reference_word, model_path, layer_id, attention_head_id):
